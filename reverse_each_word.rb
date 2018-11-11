d0abb63f19bdc05abe6b7ea4190c710aa6092b7b
Saad Malik <saad.t.malik@gmail.com>
2018-11-11 11:52:43 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 require 'pry'
 
 def reverse_each_word(string)
-  string = " "
   string.each do |words|
     words.split(" ").reverse.join(" ")
   end
   return string
 end
-  binding.pry
+binding.pry
